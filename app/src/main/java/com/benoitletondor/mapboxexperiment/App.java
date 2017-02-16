459eb2b6721a6318c6499f507d365cb605d022ff
Benoit Letondor <benoit.letondor@gmail.com>
2017-02-16 17:35:53 +0100

    Add App class

---
+++ app/src/main/java/com/benoitletondor/mapboxexperiment/App.java
@@ -0,0 +1,16 @@
+package com.benoitletondor.mapboxexperiment;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+public class App extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+        LeakCanary.install(this);
+    }
+}
